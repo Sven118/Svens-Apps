e68c2160083057925b53f08d006497c760db1d6d
Sven118 <sven118@hotmail.com>
2016-12-18 18:02:36 -0500

    Update index.js

--- moontest-master/index.js
+++ moontest-master/index.js
@@ -204,20 +204,31 @@ AFRAME.registerComponent('altspace-tracked-controls', {
   }
 });
 
-AFRAME.registerComponent('altspace-collider', {		
-         schema: { enabled: { default: true } },		
-         init: function () {		
-           var flags = {collider: {enabled: this.data.enabled}};		
-           var self = this;		
-           function setColliderFlag() {		
-             self.el.object3D.userData.altspace = flags;		
-             self.el.object3D.traverse(function (obj) {		
-               if (obj instanceof THREE.Mesh) {		
-                 obj.userData.altspace = flags;		
-               }		
-             })		
-           }		
-           setColliderFlag();		
-          this.el.addEventListener('model-loaded', setColliderFlag);		
-        }		
-      });
+AFRAME.registerComponet('synctime', {
+  
+  function syncTime() {
+    // Set up our time object, synced by the HTTP DATE header
+    // Fetch the page over JS to get just the headers
+    console.log("syncing time")
+    var r = new XMLHttpRequest();
+    var start = (new Date).getTime();
+
+    r.open('HEAD', document.location, false);
+    r.onreadystatechange = function()
+    {
+        if (r.readyState != 4)
+        {
+            return;
+        }
+        var latency = (new Date).getTime() - start;
+        var timestring = r.getResponseHeader("DATE");
+
+        // Set the time to the **slightly old** date sent from the 
+        // server, then adjust it to a good estimate of what the
+        // server time is **right now**.
+        systemtime = new Date(timestring);
+        systemtime.setMilliseconds(systemtime.getMilliseconds() + (latency / 2))
+    };
+    r.send(null);
+  }
+}
